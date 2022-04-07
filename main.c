439ee131ca084ae2755f1b44cdfcb3fc9456958e
wilsonrobotics2 <48923297+wilsonrobotics2@users.noreply.github.com>
2022-04-07 15:07:10 -0700

    Roomba code (updated)

--- main.c
+++ main.c
@@ -11,15 +11,15 @@ int main()
     create_safe();
     shut_down_in(118);
     //wait_for_light(0);
-  	//msleep(35000);
-    servo(2,500, 350);
-    servo(1, 300, 500);
+    //msleep(5000);
+    servo(2, 510, 350);
+    servo(1, 1380, 500);
     move(200, 200, 2050);
     tr(-26, 200, -200);
     move(200, 200, 550);
-    tr(-91, 200, -200);
+    tr(-86, 200, -200);
     set_create_distance(0);
-        
+
     while (get_create_distance() < 700)
     {
         if ( (get_create_lfcliff_amt() >= lth) && (get_create_rfcliff_amt() < lth) )
@@ -33,32 +33,34 @@ int main()
             create_drive_direct(90, 50);
         }
     }
-    move(250, 250, 1100);
+    move(250, 250, 1000);
     tl(91, -200, 200);
-    
+
     set_create_distance(0);
-     while (get_create_distance() < 315)
+    while (get_create_distance() < 315)
     {
         if ( (get_create_lfcliff_amt() >= lth) && (get_create_rfcliff_amt() < lth) )
         {    
-            create_drive_direct(200, 200);
+            create_drive_direct(115, 115);
         }
         else
         {
-           create_drive_direct(50, 90);
+            create_drive_direct(50, 90);
         }
     }
-    
-    servo(1, 2047, 500);
-    move(-300, -300, 2750);
+
+    move(0, 0, 2000);
+    servo(1, 1950, 500);
+    move(-300, -300, 2500);
     create_full();
-    
-    tr(-94, 200, -200);
+
+    tr(-96, 200, -200);
     //move(100, -200, 1200);
     create_safe();
-    move(200, 183, 3650);
-    servo(2, 1550, 300);
-    servo(1, 1900, 300);
+    move(200, 190, 3000);
+    servo(2, 1100, 300);
+    servo(1, 1099, 300);
+    disable_servos();
     create_disconnect();
     return 0;
 }
@@ -82,6 +84,6 @@ void tr(int r, int t, int l){
     }
 }
 void servo(int d, int e, int f){
- set_servo_position(d, e);
+    set_servo_position(d, e);
     msleep(f);
 }
